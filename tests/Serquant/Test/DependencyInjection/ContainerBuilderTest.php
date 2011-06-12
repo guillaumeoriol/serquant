9bb427bc05227111d00c8afbeed44f9c2a7b6d12
Guillaume ORIOL <goriol@serquant.com>
2011-06-12 11:50:45 +0200

    Tests moved to a 'Test' subdirectory to conform with namespace.

--- tests/Serquant/Test/DependencyInjection/ContainerBuilderTest.php
+++ tests/Serquant/Test/DependencyInjection/ContainerBuilderTest.php
@@ -2,12 +2,15 @@
 /**
  * This file is part of the Serquant library.
  *
+ * PHP version 5.3
+ *
  * @category Serquant
- * @package  Serquant
+ * @package  Test
  * @author   Guillaume Oriol <goriol@serquant.com>
  * @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @link     http://www.serquant.com/
  */
-namespace Serquant\Tests\DependencyInjection;
+namespace Serquant\Test\DependencyInjection;
 
 use Serquant\DependencyInjection\ContainerBuilder;
 
